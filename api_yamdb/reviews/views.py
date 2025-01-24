536900d833311c6d538e0c4aa69703447d05ea51
Egor Korotaev <ego_kor@mail.ru>
2025-01-24 15:33:54 +0300

    Add viewsets

--- api_yamdb/reviews/views.py
+++ api_yamdb/reviews/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Category, Comment, Genre, Review, Title
+from .permissions import (OwnerOrModerOrAdminOrSuperuserOrReadOnly,
+                          AdminOrSuperuserOrReadOnly)
+from .serializers import (CategorySerializer,
+                          CommentSerializer,
+                          GenreSerializer,
+                          ReviewSerializer,
+                          TitleSerializer)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (AdminOrSuperuserOrReadOnly,)
+    pagination_class = PageNumberPagination
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (AdminOrSuperuserOrReadOnly,)
+    pagination_class = PageNumberPagination
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (AdminOrSuperuserOrReadOnly,)
+    pagination_class = PageNumberPagination
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (OwnerOrModerOrAdminOrSuperuserOrReadOnly,)
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (OwnerOrModerOrAdminOrSuperuserOrReadOnly,)
+    pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
