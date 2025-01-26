2bacc10831ea5d32aa65a29500b3c9ea0b151d20
Egor Korotaev <ego_kor@mail.ru>
2025-01-26 11:27:48 +0300

    All category and genre tests passed

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
@@ -16,7 +16,7 @@ class Category(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
