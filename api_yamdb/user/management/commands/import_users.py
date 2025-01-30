dcba14bb7078127814c2592feba913198ac8a9f0
Aleksandr Stepanishchev <splash.dj@yandex.ru>
2025-01-31 01:10:16 +0500

    Update import_users command to import from CSV data

--- api_yamdb/user/management/commands/import_users.py
+++ api_yamdb/user/management/commands/import_users.py
@@ -15,25 +15,31 @@ class Command(BaseCommand):
         try:
             with open(csv_file, mode='r', encoding='utf-8') as file:
                 reader = csv.DictReader(file)
-                for row in reader:
-                    self_id = int(row['id'])
-                    username = str(row['username'])
-                    email = str(row['email'])
-                    role = str(row['role'])
-                    bio = str(row['bio'])
-                    first_name = str(row['first_name'])
-                    last_name = str(row['last_name'])
+                try:
+                    for row in reader:
+                        self_id = int(row['id'])
+                        username = str(row['username'])
+                        email = str(row['email'])
+                        role = str(row['role'])
+                        bio = str(row['bio'])
+                        first_name = str(row['first_name'])
+                        last_name = str(row['last_name'])
 
-                    model.objects.update_or_create(
-                        id=self_id,
-                        defaults={
-                            'username': username,
-                            'email': email,
-                            'role': role,
-                            'bio': bio,
-                            'first_name': first_name,
-                            'last_name': last_name
-                        }
+                        model.objects.update_or_create(
+                            id=self_id,
+                            defaults={
+                                'username': username,
+                                'email': email,
+                                'role': role,
+                                'bio': bio,
+                                'first_name': first_name,
+                                'last_name': last_name
+                            }
+                        )
+                except ValueError as e:
+                    self.stdout.write(
+                        self.style.ERROR(f'Ошибка в строке {row}: {e}')
                     )
+
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Произошла ошибка: {e}'))
