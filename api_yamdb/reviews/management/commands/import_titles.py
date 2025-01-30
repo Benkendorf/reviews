9869bd4fdd5c547f56e71bfcd64b77d2fde9ddb6
Aleksandr Stepanishchev <splash.dj@yandex.ru>
2025-01-31 01:14:00 +0500

    Add import_titles command to import from CSV data

---
+++ api_yamdb/reviews/management/commands/import_titles.py
@@ -0,0 +1,46 @@
+import csv
+
+from django.core.management import BaseCommand
+
+from reviews.models import Category, Title
+
+
+class Command(BaseCommand):
+    help = 'Загрузка заголовков в БД из CSV'
+
+    def handle(self, *args, **options):
+        csv_file = 'static/data/titles.csv'
+        model = Title
+
+        try:
+            with open(csv_file, mode='r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                try:
+                    for row in reader:
+                        self_id = int(row['id'])
+                        name = row['name']
+                        year = int(row['year'])
+                        category_id = self.get_category(int(row['category']))
+                        model.objects.update_or_create(
+                            id=self_id,
+                            defaults={
+                                'name': name,
+                                'year': year,
+                                'category': category_id
+                            }
+                        )
+                except ValueError as e:
+                    self.stdout.write(
+                        self.style.ERROR(f'Ошибка в строке {row}: {e}')
+                    )
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Произошла ошибка: {e}'))
+
+    @staticmethod
+    def get_category(category_id):
+        try:
+            category = Category.objects.get(id=category_id)
+            return category
+        except Category.DoesNotExist:
+            return ValueError('Категория не найдена')
