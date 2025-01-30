c50576f9e9b75002c2b565be121d64264020c4f0
Aleksandr Stepanishchev <splash.dj@yandex.ru>
2025-01-31 01:10:53 +0500

    Update import_category command to import from CSV data

--- api_yamdb/reviews/management/commands/import_category.py
+++ api_yamdb/reviews/management/commands/import_category.py
@@ -11,21 +11,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         csv_file = 'static/data/category.csv'
         model = Category
-
         try:
             with open(csv_file, mode='r', encoding='utf-8') as file:
                 reader = csv.DictReader(file)
-                for row in reader:
-                    self_id = int(row['id'])
-                    name = row['name']
-                    slug = row['slug']
-
-                    model.objects.update_or_create(
-                        id=self_id,
-                        defaults={
-                            'name': name,
-                            'slug': slug
-                        }
+                try:
+                    for row in reader:
+                        self_id = int(row['id'])
+                        name = row['name']
+                        slug = row['slug']
+                        model.objects.update_or_create(
+                            id=self_id,
+                            defaults={
+                                'name': name,
+                                'slug': slug
+                            }
+                        )
+                except ValueError as e:
+                    self.stdout.write(
+                        self.style.ERROR(f'Ошибка в строке {row}: {e}')
                     )
+
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Произошла ошибка: {e}'))
