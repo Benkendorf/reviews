7a17dd8a94d7d6510f4d12a92cda4ec5929816e2
Aleksandr Stepanishchev <splash.dj@yandex.ru>
2025-01-31 00:11:54 +0500

    Add import_users command to import from CSV data

---
+++ api_yamdb/user/management/commands/import_users.py
@@ -0,0 +1,39 @@
+import csv
+
+from django.core.management import BaseCommand
+
+from user.models import User
+
+
+class Command(BaseCommand):
+    help = 'Загрузка пользовательских данных в БД из CSV'
+
+    def handle(self, *args, **options):
+        csv_file = 'static/data/users.csv'
+        model = User
+
+        try:
+            with open(csv_file, mode='r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    self_id = int(row['id'])
+                    username = str(row['username'])
+                    email = str(row['email'])
+                    role = str(row['role'])
+                    bio = str(row['bio'])
+                    first_name = str(row['first_name'])
+                    last_name = str(row['last_name'])
+
+                    model.objects.update_or_create(
+                        id=self_id,
+                        defaults={
+                            'username': username,
+                            'email': email,
+                            'role': role,
+                            'bio': bio,
+                            'first_name': first_name,
+                            'last_name': last_name
+                        }
+                    )
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Произошла ошибка: {e}'))
